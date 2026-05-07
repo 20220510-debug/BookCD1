d954bc1f1863a284937d09198e0e22735c1223e0
ChinhQC <20220489@eaut.edu.vn>
2026-05-07 21:32:20 +0700

    Configure authentication roles and admin middleware

---
+++ app/Models/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role_id',
+        'user_code',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::creating(function (User $user) {
+            if (!$user->user_code) {
+                $nextId = (static::max('id') ?? 0) + 1;
+                $user->user_code = 'USR' . str_pad((string) $nextId, 5, '0', STR_PAD_LEFT);
+            }
+        });
+    }
+}
